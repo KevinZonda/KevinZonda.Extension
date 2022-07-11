497c471cf77e60bb817d9c87fea99efd3d658447
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-07-11 23:26:00 +0100

    fix: CA2211

--- KevinZonda.Extension/Syntax/Haskell/Funcs.cs
+++ KevinZonda.Extension/Syntax/Haskell/Funcs.cs
@@ -2,11 +2,11 @@
 
 public static class Funcs<T>
 {
-    public static Func<T[], T> Head = new(l => Methods.Head(l));
+    public readonly static Func<T[], T> Head = new(l => Methods.Head(l));
 
-    public static Func<T[], T[]> Tail = new(l => Methods.Tail(l));
+    public readonly static Func<T[], T[]> Tail = new(l => Methods.Tail(l));
 
-    public static Func<T[], T[]> Reverse = new(l => Methods.Reverse(l));
+    public readonly static Func<T[], T[]> Reverse = new(l => Methods.Reverse(l));
 
-    public static Func<dynamic, dynamic, T[]> PlusPlus = new((x, y) => Methods.PlusPlus(x, y));
+    public readonly static Func<dynamic, dynamic, T[]> PlusPlus = new((x, y) => Methods.PlusPlus(x, y));
 }
