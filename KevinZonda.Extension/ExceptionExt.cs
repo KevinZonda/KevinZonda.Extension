81dbda8c4ccc9972711e77c52844f16ecf9039cf
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-07-20 06:50:01 +0100

    feat: support ex.Is<>

---
+++ KevinZonda.Extension/ExceptionExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace KevinZonda.Extension;
+
+public static class ExceptionExt
+{
+    public static bool Is<T>() where T : Exception
+    {
+        return typeof(T) == typeof(Exception);
+    }
+}
