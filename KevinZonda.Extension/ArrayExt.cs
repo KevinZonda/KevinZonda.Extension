5bffcb8d4bae6c508c78a19de07ec9eb48970cb6
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-07-20 05:58:10 +0100

    feat: support smart index

--- KevinZonda.Extension/ArrayExt.cs
+++ KevinZonda.Extension/ArrayExt.cs
@@ -4,10 +4,13 @@ using System.Linq;
 namespace KevinZonda.Extension;
 public static class ArrayExt
 {
-    public static T? SafeIndex<T>(this T[] arr, int index)
+    /// <summary>
+    /// if out of bound, return defaultValue
+    /// </summary>
+    public static T? SafeIndex<T>(this T[] arr, int index, T? defaultValue = default)
     {
         if (arr == null) return default;
-        if (index < 0 || index >= arr.Length) return default;
+        if (index < 0 || index >= arr.Length) return defaultValue;
         return arr[index];
     }
 
@@ -16,6 +19,21 @@ public static class ArrayExt
         if (arr == null) return Array.Empty<string>();
         return arr.Select(s => s.Trim()).ToArray();
     }
+
+    /// <summary>
+    /// if index is neg, will return len + index
+    /// </summary>
+    public static T SmartIndex<T>(this T[] arr, int index)
+    {
+        return index >= 0 ? arr[index] : arr[arr.Length + index];
+    }
+
+    public static T? SmartSafeIndex<T>(this T[] arr, int index, T? defaultValue = default)
+    {
+        var nI = index >= 0 ? index : arr.Length + index;
+        return arr.SafeIndex(nI);
+    }
+
 
     public static T[] Append<T>(T[] x, T[] y)
     {
