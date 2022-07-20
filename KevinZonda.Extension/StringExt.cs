10c1107c9068a4e5a9c13e54f8f391770fd33644
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-07-20 06:56:20 +0100

    feat: support s.Count()

--- KevinZonda.Extension/StringExt.cs
+++ KevinZonda.Extension/StringExt.cs
@@ -13,4 +13,24 @@ public static class StringExt
     {
         return string.IsNullOrWhiteSpace(s);
     }
+
+    public static int Count(this string s, char ch)
+    {
+        int counter = 0;
+        foreach (var c in s)
+        {
+            if (c == ch) ++counter;
+        }
+        return counter;
+    }
+    public static int Count(this string s, string ss)
+    {
+        int count = 0, minIndex = s.IndexOf(ss, 0);
+        while (minIndex != -1)
+        {
+            minIndex = s.IndexOf(ss, minIndex + ss.Length);
+            ++count;
+        }
+        return count;
+    }
 }
